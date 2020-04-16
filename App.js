3a0a7c64418d5af8a97d21c34dc14b1b17a1c7a6
楊育瑄 <yuhsuanya@gmail.com>
2020-04-16 23:32:16 +0800

    AlbumScreen.js

--- App.js
+++ App.js
@@ -1,81 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { View, FlatList } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+import AlbumScreen from './src/screens/AlbumScreen';
+import DetailScreen from './src/screens/DetailScreen';
+import albumData from "./src/json/albums.json";
+
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.p1}>110619044</Text>
-      <Text style={styles.p2}>楊育瑄</Text>
-
-   
-      <View style={styles.square}></View>
-      <View style={styles.square1}></View>
-      <View style={styles.square2}></View>
-      <View style={styles.square3}></View>
-      <View style={styles.square4}></View>
-      <View style={styles.square5}></View>
-   </View>
-    
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Home" 
+          component={AlbumScreen} 
+          options={{
+            title: albumData.albumTitle,
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 20,
+              color:"#fff"
+            }, 
+          }}
+        />
+        <Stack.Screen 
+          name="Detail" 
+          component={DetailScreen}
+          options={({ route }) => ({ 
+            title: route.params.title,
+            headerStyle: {
+              backgroundColor: '#00b49f',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: '400',
+              fontSize: 20
+            },  
+           })}
+        />      
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFB5B5',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontFamily:'微軟正黑體',
-    
-  },
-  p1:{
-    fontSize:18,
-    color:'#666666',
-  },
-  p2:{
-  fontSize:20,
-  top:10,
-  color:'#666666',
-  },
-  square:{
-   width: 30,
-   height: 30,
-   bottom:-80,
-   backgroundColor:'#FF8888',
-  },
-  square1:{
-  width: 30,
-  height: 30,
-  bottom:-40,
-  right:20,
-  backgroundColor:'#FF8888',
-  },
-  square2:{
-  width: 30,
-  height: 30,
-  bottom:-0,
-  right:40,
-  backgroundColor:'#FF8888',
-  },
-  square3:{
-  width: 30,
-  height: 30,
-  bottom:20,
-  right:-20,
-  backgroundColor:'#FF8888',
-  },
-  square4:{
-  width: 30,
-  height: 30,
-  bottom:60,
-  right:-40,
-  backgroundColor:'#FF8888',
-  },
-  square5:{
-    width: 30,
-    height: 30,
-    bottom:40,
-    
-    backgroundColor:'#FF8888',
-    },
-});
+export default App;
